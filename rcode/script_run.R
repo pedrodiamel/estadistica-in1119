a0e36673796a287f43e47ea170bc89282a123dfe
pedrodiamel <pedrodiamel@gmail.com>
2015-11-27 15:25:43 -0300

    add new graph

--- rcode/script_run.R
+++ rcode/script_run.R
@@ -92,8 +92,11 @@ m + geom_histogram(alpha=0.8, binwidth = 1) +
 
 ## Analisis individual
 
-X = db$PS # PowerSave
+
+# Power Save --------------------------------------------------------------
+
 name_alg = "PS"
+PS = db$PS
 
 #Histograma (500x400)
 ggplot(db, aes(x=PS)) +
@@ -113,7 +116,7 @@ ggplot(db, aes(x=PS)) +
 #Q-Q
 ggplot(db, aes(sample = db$PS)) + 
   stat_qq() +
-  ylab("PS")
+  ylab(name_alg)
 
 #Box-plot
 ggplot(NULL, aes(x=1, y=PS)) + 
@@ -123,10 +126,246 @@ ggplot(NULL, aes(x=1, y=PS)) +
 
 
 #Test Adherencia
-test_adh = st.test.adherencia(PS)
+test_adh = st.test.adherencia(db$PS)
 test_adh$shapiro
 test_adh$lillie
 test_adh$anderson
+
+
+ 
+
+# ANALISIS DE OnDemand ----------------------------------------------------
+
+name_alg = "O"
+O = db$O
+
+
+#Histograma (500x400)
+ggplot(db, aes(x=O)) +
+  geom_histogram(
+    aes(y=..density..),
+    binwidth = 2,
+    colour="black", 
+    fill="white" 
+  ) +
+  stat_function(fun=dnorm, 
+                args=list(mean=mean(db$O), sd=sd(db$O)), 
+                color ="red") +
+  xlab(name_alg)
+#geom_line(stat="density", adjust=.55)
+
+#png
+# png(file="ohistnorm.png",height=500,width=(500))
+# dev.off()
+ggsave("graph/ohistnorm.png", width=4, height=4, dpi=300)
+
+#Q-Q
+ggplot(db, aes(sample = db$O)) + 
+  stat_qq() +
+  ylab(name_alg)
+
+ggsave("graph/oqqnorm.png", width=4, height=4, dpi=300)
+
+
+#Box-plot
+ggplot(NULL, aes(x=1, y=O)) + 
+  geom_boxplot(notch=TRUE) +
+  scale_x_continuous(breaks=NULL) +
+  theme(axis.title.x = element_blank())
+
+
+#Test Adherencia
+test_adh = st.test.adherencia(db$O)
+test_adh$shapiro
+test_adh$lillie
+test_adh$anderson
+
+
+
+
+# ANALISIS DE Performance  ------------------------------------------------
+
+name_alg = "P"
+P = db$P
+
+#Histograma (500x400)
+ggplot(db, aes(x=P)) +
+  geom_histogram(
+    aes(y=..density..),
+    binwidth = 2,
+    colour="black", 
+    fill="white" 
+  ) +
+  stat_function(fun=dnorm, 
+                args=list(mean=mean(db$P), sd=sd(db$P)), 
+                color ="red") +
+  xlab(name_alg)
+#geom_line(stat="density", adjust=.55)
+
+ggsave("graph/phistnorm.png", width=4, height=4, dpi=300)
+
+
+
+#Q-Q
+ggplot(db, aes(sample = db$P)) + 
+  stat_qq() +
+  ylab(name_alg)
+
+ggsave("graph/pqqnorm.png", width=4, height=4, dpi=300)
+
+
+#Box-plot
+ggplot(NULL, aes(x=1, y=P)) + 
+  geom_boxplot(notch=TRUE) +
+  scale_x_continuous(breaks=NULL) +
+  theme(axis.title.x = element_blank())
+
+
+#Test Adherencia
+test_adh = st.test.adherencia(db$P)
+test_adh$shapiro
+test_adh$lillie
+test_adh$anderson
+
+
+
+
+# ANALISIS DE PegasusQ ----------------------------------------------------
+
+name_alg = "PQ"
+PQ = db$PQ
+
+#Histograma (500x400)
+ggplot(db, aes(x=PQ)) +
+  geom_histogram(
+    aes(y=..density..),
+    binwidth = 1,
+    colour="black", 
+    fill="white" 
+  ) +
+  stat_function(fun=dnorm, 
+                args=list(mean=mean(db$PQ), sd=sd(db$PQ)), 
+                color ="red") +
+  xlab(name_alg)
+#geom_line(stat="density", adjust=.55)
+
+ggsave("graph/pqhistnorm.png", width=4, height=4, dpi=300)
+
+
+#Q-Q
+ggplot(db, aes(sample = db$PQ)) + 
+  stat_qq() +
+  ylab(name_alg)
+
+ggsave("graph/pqqqnorm.png", width=4, height=4, dpi=300)
+
+#Box-plot
+ggplot(NULL, aes(x=1, y=PQ)) + 
+  geom_boxplot(notch=TRUE) +
+  scale_x_continuous(breaks=NULL) +
+  theme(axis.title.x = element_blank())
+
+
+#Test Adherencia
+test_adh = st.test.adherencia(db$PQ)
+test_adh$shapiro
+test_adh$lillie
+test_adh$anderson
+
+
+
+# X700MHz -----------------------------------------------------------------
+
+name_alg = "X700MHz"
+X700MHz = db$X700MHz
+
+#Histograma (500x400)
+ggplot(db, aes(x=X700MHz)) +
+  geom_histogram(
+    aes(y=..density..),
+    binwidth = 1,
+    colour="black", 
+    fill="white" 
+  ) +
+  stat_function(fun=dnorm, 
+                args=list(mean=mean(db$X700MHz), sd=sd(db$X700MHz)), 
+                color ="red") +
+  xlab(name_alg)
+#geom_line(stat="density", adjust=.55)
+
+ggsave("graph/x700histnorm.png", width=4, height=4, dpi=300)
+
+
+
+#Q-Q
+ggplot(db, aes(sample = db$X700MHz)) + 
+  stat_qq() +
+  ylab(name_alg)
+
+ggsave("graph/x700qqnorm.png", width=4, height=4, dpi=300)
+
+#Box-plot
+ggplot(NULL, aes(x=1, y=X700MHz)) + 
+  geom_boxplot(notch=TRUE) +
+  scale_x_continuous(breaks=NULL) +
+  theme(axis.title.x = element_blank())
+
+
+#Test Adherencia
+test_adh = st.test.adherencia(db$X700MHz)
+test_adh$shapiro
+test_adh$lillie
+test_adh$anderson
+
+
+# X1100MHz ----------------------------------------------------------------
+
+name_alg = "X1100MHz"
+X1100MHz = db$X1100MHz
+
+#Histograma (500x400)
+ggplot(db, aes(x=X1100MHz)) +
+  geom_histogram(
+    aes(y=..density..),
+    binwidth = 1,
+    colour="black", 
+    fill="white" 
+  ) +
+  stat_function(fun=dnorm, 
+                args=list(mean=mean(db$X1100MHz), sd=sd(db$X1100MHz)), 
+                color ="red") +
+  xlab(name_alg)
+#geom_line(stat="density", adjust=.55)
+
+ggsave("graph/X1100MHzhistnorm.png", width=4, height=4, dpi=300)
+
+
+
+#Q-Q
+ggplot(db, aes(sample = db$X1100MHz)) + 
+  stat_qq() +
+  ylab(name_alg)
+
+ggsave("graph/X1100MHzqqnorm.png", width=4, height=4, dpi=300)
+
+#Box-plot
+ggplot(NULL, aes(x=1, y=X1100MHz)) + 
+  geom_boxplot(notch=TRUE) +
+  scale_x_continuous(breaks=NULL) +
+  theme(axis.title.x = element_blank())
+
+
+#Test Adherencia
+test_adh = st.test.adherencia(db$X1100MHz)
+test_adh$shapiro
+test_adh$lillie
+test_adh$anderson
+
+
+# X1400MHz ----------------------------------------------------------------
+
+
+
 
 
 
